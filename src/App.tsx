37833eb1304ea9235f4e616b96217b9e713a2927
redesyef <ymauriciop@gmail.com>
2022-06-15 07:34:41 -0500

    maquetación login

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
 import Auth from "./pages/Auth";
+import EmailSent from "./pages/EmailSent";
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <div>
+      <EmailSent/>
       <Auth />
     </div>
   );
---
+++ src/pages/EmailSent.tsx
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+import "./Auth.css";
+import logo from "../image/Logos/logo_yard_sale.svg"
+import email from "../image/icons/email.svg"
+const EmailSent = () => {
+  const [datos, setDatos] = useState("");
+  const data = (value:string) => {
+    console.log(value);
+  };
+ 
+  return (
+    <div>
+      <div>
+        <div className="login">
+          <div className="form-container">
+            <img src={logo} alt="logo" className="logo" />
+            <h1 className="title">Crear nueva contraseña</h1>
+            <p className="subtitle">Ingresar nueva contraseña</p>
+            <form action="" className="form">
+            <label htmlFor="password" className="label">
+              Contraseña
+            </label>
+            <input
+              type="password"
+              className="input input-pasword"
+              id="password"
+              placeholder="********"
+            />
+            
+            <label htmlFor="new-password" className="label">
+              Repetir contraseña
+            </label>
+            <input
+              type="password"
+              className="input input-pasword"
+              id="new-password"
+              placeholder="********"
+            />
+            <input
+              type="submit"
+              value="Confirmar"
+              className="primary-button login-button"
+            />
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default EmailSent;
